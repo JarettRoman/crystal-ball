9b23d02f158d04d30f29c3ef47fa1817750749a0
Jarett <jarettroman@live.com>
2013-06-16 04:29:29 -0400

    added sound effects, animation, and began messing with accelerometer.

--- src/com/example/crystal/ball/MainActivity.java
+++ src/com/example/crystal/ball/MainActivity.java
@@ -1,15 +1,29 @@
 package com.example.crystal.ball;
 
+import com.example.crystal.ball.ShakeDetector.OnShakeListener;
+
 import android.app.Activity;
+import android.graphics.drawable.AnimationDrawable;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.View;
-import android.widget.Button;
+import android.view.animation.AlphaAnimation;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
 	private CrystalBall mCrystalBall = new CrystalBall();
+	private TextView mAnswerLabel;
+	
+	private ImageView mCrystalBallImage;
+	private SensorManager mSensorManager;
+	private Sensor mAccelerometer;
+	private ShakeDetector mShakeDetector;
+	
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,19 +34,60 @@ public class MainActivity extends Activity {
          * them the Views form the layout file
          */
         
-        final TextView answerLabel = (TextView) findViewById(R.id.textView1);
-        Button getAnswerButton = (Button) findViewById(R.id.button1);
+        mAnswerLabel = (TextView) findViewById(R.id.textView1);
+        mCrystalBallImage = (ImageView) findViewById(R.id.imageView1);
         
-        getAnswerButton.setOnClickListener(new View.OnClickListener() {
+        //setting up button with a listener that does an action upon clicking said button.
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mShakeDetector = new ShakeDetector(new OnShakeListener() {
 			
 			@Override
-			public void onClick(View v) {
+			public void onShake() {
 				
-				String answer = mCrystalBall.getAnAnswer();
+				handleNewAnswer();
 				
-				
-				
-				answerLabel.setText(answer);
+			}
+		});
+       
+        
+        
+        mSensorManager.registerListener(mShakeDetector, mAccelerometer, 
+        		SensorManager.SENSOR_DELAY_UI);
+    }
+    
+    
+    //sets up animation sequence for the Crystal Ball
+    private void animateCrystalBall() {
+    	
+    	mCrystalBallImage.setImageResource(R.drawable.ball_animation);
+    	AnimationDrawable ballAnimation = (AnimationDrawable) mCrystalBallImage.getDrawable();
+    	if(ballAnimation.isRunning()){
+    		ballAnimation.stop();
+    	}
+    	ballAnimation.start();
+    	
+    }
+    
+    //sets up the animation sequence for the answer text
+    private void animateAnswer() {
+    	AlphaAnimation fadeInAnimation = new AlphaAnimation(0,1);
+    	fadeInAnimation.setDuration(1500);
+    	fadeInAnimation.setFillAfter(true);
+    	
+    	mAnswerLabel.setAnimation(fadeInAnimation);
+    }
+    
+    
+    //sets up the chime played when receiving a fortune.
+    private void playSound(){
+    	MediaPlayer player = MediaPlayer.create(this, R.raw.crystal_ball);
+    	player.start();
+    	player.setOnCompletionListener(new OnCompletionListener() {
+			
+			@Override
+			public void onCompletion(MediaPlayer mp) {
+				mp.release();
 				
 			}
 		});
@@ -43,4 +98,17 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+
+
+	private void handleNewAnswer() {
+		String answer = mCrystalBall.getAnAnswer();
+		
+		
+		
+		mAnswerLabel.setText(answer);
+		
+		animateCrystalBall();
+		animateAnswer();
+		playSound();
+	}
 }
